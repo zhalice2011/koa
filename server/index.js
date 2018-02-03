cf71d6e2fab41879fa049e7cd2bfd78b7057df96
zhalice2011 <zhalice2012@gmail.com>
2018-02-03 08:47:40 +0800

    koa路由中间件

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const koa = require('koa')
+const logger = require('koa-logger')
 const app = new koa()
+
 
 // 中间件1
 const mid1 = async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf8'
+  ctx.body = '你好'
+  
   await next()
+  
+  ctx.body = ctx.body + '这里'
+  
 }
 // 中间件2
 const mid2 = async (ctx, next) => {
-  ctx.body = '你好'
+  ctx.type = 'text/html; charset=utf8'
+  
   await next()
 }
 // 中间件3
@@ -16,6 +23,28 @@ const mid3 = async (ctx, next) => {
   ctx.body = ctx.body + 'Loser'
   await next()
 }
+
+function pure(x) {
+  return x + 1
+}
+
+// function tail(i) {
+//   if (i > 3) return 
+//   console.log('修改前', i)
+//   tail(i + 1)
+//   console.log('修改后', i)
+// }
+// tail(0)
+function tail2(i) {
+  if (i > 3) return 
+  console.log('修改前', i)
+  return tail2(i + 1)
+  console.log('修改后', i)
+}
+tail2(0)
+
+app.use(logger())
+
 
 app.use(mid1)
 app.use(mid2)
